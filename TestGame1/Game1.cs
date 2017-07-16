daae0f03a5d752ba2995c11d872e6db6f9c67f67
LeftofZen <bigbadbennybrother@gmail.com>
2017-07-16 20:56:07 +1000

    Add project files.

---
+++ TestGame1/Game1.cs
@@ -0,0 +1,198 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using System;
+using System.Collections.Generic;
+
+namespace TestGame1
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager mGraphics;
+        SpriteBatch mSpriteBatch;
+
+        Dictionary<String, Texture2D> mTextureAtlas;
+
+        //WorldGen mWorldGen;
+        World mWorld;
+
+        //Ship mShip;        
+
+        // Keyboard states used to determine key presses
+        KeyboardState currentKeyboardState;
+        KeyboardState previousKeyboardState;
+
+        // Gamepad states used to determine button presses
+        GamePadState currentGamePadState;
+        GamePadState previousGamePadState;
+
+        //Mouse states used to track Mouse button press
+        MouseState currentMouseState;
+        MouseState previousMouseState;
+
+        Vector2 keyVec;
+
+        public Game1()
+        {
+            mGraphics = new GraphicsDeviceManager(this);
+            mGraphics.IsFullScreen = false;
+            mGraphics.PreferredBackBufferWidth = 1440;
+            mGraphics.PreferredBackBufferHeight = 810;
+
+            mTextureAtlas = new Dictionary<string, Texture2D>();
+
+            this.IsMouseVisible = true;
+
+            Content.RootDirectory = "Content";
+
+            //mShip = new Ship();
+            keyVec = new Vector2();
+
+            currentMouseState = new MouseState();
+            previousMouseState = currentMouseState;
+
+            currentKeyboardState = new KeyboardState();
+            previousKeyboardState = currentKeyboardState;
+
+            currentGamePadState = new GamePadState();
+            previousGamePadState = currentGamePadState;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            //mWorldGen = new WorldGen();
+            mWorld = new World();
+
+            base.Initialize();
+        }
+
+        void LoadImage(String name)
+        {
+            mTextureAtlas.Add(name, Content.Load<Texture2D>("Sprites\\" + name));
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            mSpriteBatch = new SpriteBatch(GraphicsDevice);
+
+            LoadImage("test_village");
+            LoadImage("arrow");
+            LoadImage("dodonpachi_gas13");
+
+            mWorld.LoadContent(GraphicsDevice, mTextureAtlas);
+            //mWorldGen.LoadContent(GraphicsDevice, mTextureAtlas);
+
+            // TODO: use this.Content to load your game content here
+            //mShip.SetTexture(mTextureAtlas["dodonpachi_gas13"]);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Read the current state of the keyboard and gamepad and store it
+            currentKeyboardState = Keyboard.GetState();
+            currentGamePadState = GamePad.GetState(PlayerIndex.One);
+                  
+            if (currentGamePadState.Buttons.Back == ButtonState.Pressed || currentKeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Exit();
+            }
+
+            // Update state here
+            //
+            if (currentGamePadState.IsConnected)
+            {
+                //mShip.ProcessInputVector(currentGamePadState.ThumbSticks.Left);
+            }
+
+            // Process keyboard
+            {
+
+                if (currentKeyboardState.IsKeyDown(Keys.Left))
+                {
+                    keyVec.X -= 1;
+                }
+                if (currentKeyboardState.IsKeyDown(Keys.Right))
+                {
+                    keyVec.X += 1;
+                }
+                if (currentKeyboardState.IsKeyDown(Keys.Up))
+                {
+                    keyVec.Y -= 1;
+                }
+                if (currentKeyboardState.IsKeyDown(Keys.Down))
+                {
+                    keyVec.Y += 1;
+                }
+
+                //mWorldGen.ProcessInput();
+                if (!currentKeyboardState.IsKeyDown(Keys.Space) && previousKeyboardState.IsKeyDown(Keys.Space))
+                {
+                }
+
+                //mShip.ProcessInputVector(keyVec);
+            }
+
+
+            // Save the previous state of the keyboard and game pad so we can determine single key/button presses
+            previousGamePadState = currentGamePadState;
+            previousKeyboardState = currentKeyboardState;
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // PointClamp means nearest-neighbour interpolation
+            mSpriteBatch.Begin(
+                SpriteSortMode.Deferred, null, SamplerState.PointClamp, null, null, null, null);
+
+
+            //System.Console.WriteLine(min);
+            //System.Console.WriteLine(max);
+            //mShip.Draw(spriteBatch, gameTime);
+            //mWorldGen.Draw(gameTime, mSpriteBatch);
+            mWorld.Draw(gameTime, mSpriteBatch);
+
+            mSpriteBatch.End();
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ TestGame1/World.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TestGame1
+{
+    enum TileContent
+    { Grass, Road, Villge, Water, Lake, Forest, Farm, Paddock, Mine, Shop };
+
+    class AI
+    {
+        // intrinsic
+        Vector2 position;
+
+        // specific
+        float hunger = 0f;
+        float thirst = 0f;
+        float tiredness = 0f;
+
+    }
+
+    class WorldTile
+    {
+        public Color colour = Color.White;
+        public bool isWalkable = false;
+    }
+
+    class World
+    {
+        Dictionary<Color, TileContent> mMapDefinition; // maps colours to the meaning of that colour
+        Dictionary<String, Texture2D> mTextureAtlas;
+        Texture2D mPixel;
+        //Texture2D mInputImage;
+        RenderTarget2D mMap;
+        GraphicsDevice mGraphicsDevice;
+
+        WorldTile[,] mWorld; // = new WorldTile[64, 64];
+
+        int mWorldSizeX = 0;
+        int mWorldSizeY = 0;
+        int mGridSize = 8;
+        int mWinWidth = 0;
+        int mWinHeight = 0;
+
+        public void LoadContent(GraphicsDevice graphicsDevice, Dictionary<String, Texture2D> textureAtlas)
+        {
+            // set private members
+            mGraphicsDevice = graphicsDevice;
+            mTextureAtlas = textureAtlas;
+
+            // create a 'pixel' texture
+            mPixel = new Texture2D(mGraphicsDevice, mGridSize, mGridSize);
+
+            // convert input texture into our useful 2d array
+            Texture2D inputImage = mTextureAtlas["test_village"];
+
+            mWorldSizeX = inputImage.Width;
+            mWorldSizeY = inputImage.Height;
+            mWorld = new WorldTile[mWorldSizeX, mWorldSizeY];
+
+            Color[] colors1D = new Color[mWorldSizeX * mWorldSizeY];
+            inputImage.GetData<Color>(colors1D);
+            for (int y = 0; y < mWorldSizeY; y++)
+            {
+                for (int x = 0; x < mWorldSizeX; x++)
+                {
+                    mWorld[x, y] = new WorldTile();
+                    mWorld[x, y].colour = colors1D[x + y * mWorldSizeX];
+                }
+            }
+
+            // populate the texture with white
+            List<Color> colorList = new List<Color>();
+            for (int i = 0; i < mGridSize * mGridSize; ++i)
+                colorList.Add(Color.White);
+            mPixel.SetData(colorList.ToArray());
+
+            mWinWidth = mGraphicsDevice.PresentationParameters.BackBufferWidth;
+            mWinHeight = mGraphicsDevice.PresentationParameters.BackBufferHeight;
+
+            // map
+            mMap = new RenderTarget2D(
+                mGraphicsDevice,
+                mWinWidth,
+                mWinHeight);
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            for (int y = 0; y < mWorldSizeY; y++)
+            {
+                for (int x = 0; x < mWorldSizeX; x++)
+                {
+                    spriteBatch.Draw(
+                        mPixel,
+                        new Vector2(x * mGridSize, y * mGridSize),
+                        null,
+                        null,
+                        null,
+                        0f,
+                        Vector2.One,
+                        mWorld[x, y].colour,
+                        SpriteEffects.None,
+                        0f);
+                }
+            }
+
+            // scale 8 times
+            //spriteBatch.Draw(mInputImage, Vector2.Zero, null, Color.White, 0f, Vector2.Zero, 8f, SpriteEffects.None, 0);
+        }
+    }
+}
